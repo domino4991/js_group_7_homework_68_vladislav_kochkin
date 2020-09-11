27049ff7997e5dd5bfc0646a324c168ab1826d93
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-09-11 20:13:42 +0600

    Добавил страницу с тасками

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Counter from "../Counter/Counter";
 import Layout from "../../components/Layout/Layout";
 import {Route, Switch} from "react-router-dom";
+import TodoList from "../TodoList/TodoList";
 
 function App() {
   return (
       <Layout>
         <Switch>
-          <Route path="/" component={Counter} />
-          {/*<Route path="/todo" component={}*/}
+          <Route path="/" exact component={Counter} />
+          <Route path="/todo" component={TodoList} />
           <Route render={() => <h1 style={{textAlign: 'center'}}>404 page not found</h1>} />
         </Switch>
       </Layout>
