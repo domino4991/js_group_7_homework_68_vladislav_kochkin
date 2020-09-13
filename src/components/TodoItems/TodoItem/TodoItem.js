ecc62232854a4db15ef218418f0ee0a31d4acc03
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-09-13 19:46:40 +0600

    Оформление тасков и формы для добавления тасков

--- src/components/TodoItems/TodoItem/TodoItem.js
+++ src/components/TodoItems/TodoItem/TodoItem.js
@@ -3,16 +3,18 @@ import './TodoItem.css';
 
 const TodoItem = props => {
     return (
-        <div className="Todo-item">
-            <input
-                type="checkbox"
-                className="Todo-item__done-task"
-                name="done"
-                id={props.id}
-                checked={props.checked}
-                onChange={props.checkedSwitch}
-            />
-            <p className="Todo-item__task">{props.task}</p>
+        <div className={props.checked ? 'Todo-item checked' : 'Todo-item'}>
+            <div className="Todo-item__task-box">
+                <input
+                    type="checkbox"
+                    className="Todo-item__done-task"
+                    name="done"
+                    id={props.id}
+                    checked={props.checked}
+                    onChange={props.checkedSwitch}
+                />
+                <label htmlFor={props.id} className="Todo-item__task">{props.task}</label>
+            </div>
             <button className="Todo-item__delete" onClick={props.clicked}>Delete</button>
         </div>
     );
